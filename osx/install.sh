aaef562e463e45d2ba6010bf387276d2ee5a1d13
Spencer Fitzgerald <sfitzgerald@ibm.com>
2022-10-25 12:46:00 -0600

    formatting

--- osx/install.sh
+++ osx/install.sh
@@ -7,12 +7,21 @@ fi
 # Install Homebrew
 if ! command -v brew &> /dev/null
 then
-  echo "Installing Homebrew..."
+  echo "====================="
+  echo "Installing Homebrew"
+  echo "====================="
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo "====================="
+  echo "Finished Homebrew"
+  echo -e "=====================\n"
 fi
 
-echo "Running brew bunlde..."
-brew bundle
+echo "====================="
+echo "Running brew bundle"
+echo "====================="
+brew bundle --file "~/dotfiles/osx/Brewfile"
+echo "====================="
+echo "Finished brew bundle"
+echo -e "=====================\n"
 
-echo "Running install on shared directory"
 ./shared/install.sh
--- shared/install.sh
+++ shared/install.sh
@@ -1,24 +1,45 @@
 #!/bin/bash
+if [ ! -f $HOME/.gitconfig ]; then
+  echo "====================="
+  echo "Configuring Git"
+  echo "====================="
 
-echo "Configuring Git..."
-echo -n "  Name: "
-read GIT_NAME
-echo -n "  Email: "
-read GIT_EMAIL
+  # Collect inputs
+  echo -n "  Name: "
+  read GIT_NAME
+  echo -n "  Email: "
+  read GIT_EMAIL
 
-git config --global user.name "$GIT_NAME"
-git config --global user.email "$GIT_EMAIL"
-git config --global alias.lb "!git reflog show --pretty=format:'%gs ~ %gd' --date=relative | grep 'checkout:' | grep -oE '[^ ]+ ~ .*' | awk -F~ '!seen[\$1]++' | head -n 10 | awk -F' ~ HEAD@{' '{printf(\"  \\033[33m%s: \\033[37m %s\\033[0m\\n\", substr(\$2, 1, length(\$2)-1), \$1)}'"
+  git config --global user.name "$GIT_NAME"
+  git config --global user.email "$GIT_EMAIL"
+  git config --global alias.lb "!git reflog show --pretty=format:'%gs ~ %gd' --date=relative | grep 'checkout:' | grep -oE '[^ ]+ ~ .*' | awk -F~ '!seen[\$1]++' | head -n 10 | awk -F' ~ HEAD@{' '{printf(\"  \\033[33m%s: \\033[37m %s\\033[0m\\n\", substr(\$2, 1, length(\$2)-1), \$1)}'"
+
+  echo "====================="
+  echo "Finished Configuring Git"
+  echo -e "=====================\n"
+fi
 
 # Install oh-my-zsh
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+if [ ! -d $HOME/.oh-my-zsh ]; then
+  echo "====================="
+  echo "Installing Oh-My-Zsh"
+  echo "====================="
 
-# Link the directories
-echo "Creatng Symlinks to dotfiles"
+  sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+  echo "====================="
+  echo "Finished installing Oh-My-Zsh"
+  echo -e "=====================\n"
+fi
+
+echo "====================="
+echo "Creating symlinks to dotfiles"
+echo "====================="
+
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# vim
 ln -s ${BASEDIR}/.vimrc ~/.vimrc
-
-# zsh
 ln -s ${BASEDIR}/.zshrc ~/.zshrc
+
+echo "====================="
+echo "Finished creatng symlinks to dotfiles"
+echo -e "=====================\n"

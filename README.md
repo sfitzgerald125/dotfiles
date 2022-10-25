a03a102425453a0e577c11c3428177c60de215cc
Spencer Fitzgerald <sfitzgerald@ibm.com>
2022-10-25 12:27:00 -0600

    instal oh-my-zsh

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# dotfiles
+# Getting Started
+Clone this repo into your home directory, and then run the install scripts depending on the OS.
+### Linux
+
+From the root directory, run `./linux/install.sh`
+
+### OSX
+
+From the root directory, run `./osx/install.sh`
+
+### Shared
+
+After running the OS specific packages, OS agnostic config is linked from the shared folder
--- shared/install.sh
+++ shared/install.sh
@@ -10,7 +10,11 @@ git config --global user.name "$GIT_NAME"
 git config --global user.email "$GIT_EMAIL"
 git config --global alias.lb "!git reflog show --pretty=format:'%gs ~ %gd' --date=relative | grep 'checkout:' | grep -oE '[^ ]+ ~ .*' | awk -F~ '!seen[\$1]++' | head -n 10 | awk -F' ~ HEAD@{' '{printf(\"  \\033[33m%s: \\033[37m %s\\033[0m\\n\", substr(\$2, 1, length(\$2)-1), \$1)}'"
 
+# Install oh-my-zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
 # Link the directories
+echo "Creatng Symlinks to dotfiles"
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # vim

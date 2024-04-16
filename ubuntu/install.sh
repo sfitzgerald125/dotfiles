4f7f33bd308e257a9547021213a4bed2753c9139
Spencer Fitzgerald <spencerfitz0196@gmail.com>
2024-04-16 13:21:57 -0400

    add

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -16,6 +16,8 @@ ln -s ${BASEDIR}/.tmux.conf ~/.tmux.conf
 ln -s ${BASEDIR}/.zshrc ~/.zshrc
 ln -s ${BASEDIR}/vs-code/settings.json ~/.config/Code/User/settings.json
 ln -s ${BASEDIR}/vs-code/keybindings.json ~/.config/Code/User/keybindings.json
+mkdir -p ~/.config/regolith3/i3/config.d
+ln -s ${BASEDIR}/custom-i3 ~/.config/regolith3/i3/config.d/custom-i3
 
 echo "Downloading packages from source..."
 ./linux/downloads-from-source.sh
